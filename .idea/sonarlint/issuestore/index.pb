dae1d39f4c1f4dbe9ddb6dce4a4dfc4a9e7c1054
Dorinonilov <Dorinonilov@gmailcom>
2022-04-02 20:55:09 +0300

    My first application

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+A
+src/Question.java,1\d\1dae54ed599508a1c06bc181f507883a42e5fc62
+?
+src/Answer.java,e\5\e599ce9c0fe51a2b68fa90d88245da42eb74207f
+G
+src/QuestionAnswer.java,7\f\7fdd05d332e74b7801c67c28d6b45eeccfb094bc
---
+++ src/Answer.java
@@ -0,0 +1,12 @@
+public class Answer {
+
+    String answerText;
+    boolean answerIsCorrect;
+    String answerSequence;
+
+    public Answer(String answerText, boolean answerIsCorrect, String answerSequence) {
+        this.answerText = answerText;
+        this.answerIsCorrect = answerIsCorrect;
+        this.answerSequence = answerSequence;
+    }
+}
